ef813cec2f54252889dd095cc3616d53c268b3d5
Kasper <kasper.mustad.juell@linux.com>
2024-07-01 18:55:20 +0200

    Uses input args to get the domain default is nrk.no

--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,29 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 
 	"github.com/kmuju/goDNS/internal"
 )
 
 func main() {
-	err := internal.QueryDomain()
-	if err != nil {
-		fmt.Printf("%s\n", err.Error())
+	_ = flag.String("domain", "nrk.no", "")
+	flag.Parse()
+	fmt.Printf("%+v\n", flag.Args())
+	if flag.NArg() == 0 {
+		fmt.Printf("need more arguments")
+		return
 	}
+	domain := flag.Arg(0)
+	ip, err := internal.GetAddress(domain)
+	if err == internal.OverMaxQueries {
+		fmt.Printf("Overstepped maxqueries, last ip %s\n", ip)
+		return
+	}
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("Ip address: %s\n", ip)
 }
