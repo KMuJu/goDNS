d5ee6d4109ddb736a32e21b1ff0354b5084ffcee
Kasper <kasper.mustad.juell@linux.com>
2024-07-01 16:21:09 +0200

    Added tests for parsing and helper functions

---
+++ internal/helpers_test.go
@@ -0,0 +1,62 @@
+package internal
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPointerValue(t *testing.T) {
+	cases := []struct {
+		name     string
+		pointer  [2]byte
+		expected int
+	}{
+		{
+			name:     "Test 1",
+			pointer:  [2]byte{0xc0, 0x2c},
+			expected: 44,
+		},
+		{
+			name:     "Test 2",
+			pointer:  [2]byte{0xc0, 0x17},
+			expected: 23,
+		},
+		{
+			name:     "Not A pointer",
+			pointer:  [2]byte{0x00, 0x2c},
+			expected: -1,
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			ouput := pointerValue(tc.pointer)
+			assert.Equal(t, tc.expected, ouput)
+		})
+	}
+}
+
+func TestPrefixToNull(t *testing.T) {
+	cases := []struct {
+		name          string
+		input         []byte
+		expectedbytes []byte
+		expectedlen   int
+	}{
+		{
+			name:          "Simple test",
+			input:         []byte{0x22, 0x32, 0x00, 0x43, 0x1},
+			expectedbytes: []byte{0x22, 0x32, 0x00},
+			expectedlen:   3,
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			b, l := getPrefixToNull(tc.input)
+			assert.Equal(t, tc.expectedbytes, b)
+			assert.Equal(t, tc.expectedlen, l)
+		})
+	}
+}
