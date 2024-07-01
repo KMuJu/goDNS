cf74f8c7537e48b96a50227f5d49a1402407baa1
Kasper <kasper.mustad.juell@linux.com>
2024-07-01 17:58:24 +0200

    feat: iterativ search for ip address

--- internal/socket.go
+++ internal/socket.go
@@ -3,54 +3,45 @@ package internal
 import (
 	"fmt"
 	"net"
+	"time"
 )
 
 const (
 	TYPE    = "udp"
-	ADDRESS = "198.41.0.4:53"
+	ADDRESS = "198.41.0.4"
 	PORT    = 53
 )
 
-func QueryDomain() error {
+func QueryDomain(address string, mess Message) (Message, error) {
 
-	conn, err := net.Dial(TYPE, ADDRESS)
+	conn, err := net.Dial(TYPE, fmt.Sprintf("%s:53", address))
 	if err != nil {
 		fmt.Printf("Could not connect to address\n%s\n", err.Error())
-		return err
+		return Message{}, err
 	}
 	defer conn.Close()
+	err = conn.SetDeadline(time.Now().Add(time.Second))
 
-	ex := ExampleMessage(22)
-	b, err := ex.Bytes()
+	b, err := mess.Bytes()
 	if err != nil {
-		return err
+		return Message{}, err
 	}
-	fmt.Printf("Flags: %d\n", ex.Header.flags)
 	_, err = conn.Write(b)
 	if err != nil {
-		return err
+		return Message{}, err
 	}
 
 	response := make([]byte, 1024) // Adjust buffer size as needed
 	n, err := conn.Read(response)
 	if err != nil {
-		return err
+		return Message{}, err
 	}
 
-	fmt.Printf("Received %d bytes response:\n%x\n", n, response[:n])
+	// fmt.Printf("Received %d bytes response:\n%x\n", n, response[:n])
 	m, err := Parse(response[:n])
 	if err != nil {
-		return err
+		return Message{}, err
 	}
-	b, err = m.Bytes()
-	if err != nil {
-		return err
-	}
-	fmt.Printf("Header\n %+v\n", m.Header)
-	fmt.Printf("Question(%d)\n %+v\n", len(m.Question), m.Question)
-	fmt.Printf("Answer(%d)\n %s\n", len(m.Answer), m.Answer)
-	fmt.Printf("Authority(%d)\n %s\n", len(m.Authority), m.Authority)
-	fmt.Printf("Additional(%d)\n%s\n", len(m.Additional), m.Additional)
 
-	return err
+	return m, err
 }
