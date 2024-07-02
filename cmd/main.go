124605b34bd7c91a4e49fe12db7b8da52d4b96c4
Kasper <kasper.mustad.juell@linux.com>
2024-07-02 12:34:29 +0200

    Add ability to query for multiple domains

--- cmd/main.go
+++ cmd/main.go
@@ -8,22 +8,26 @@ import (
 )
 
 func main() {
-	_ = flag.String("domain", "nrk.no", "")
 	flag.Parse()
-	fmt.Printf("%+v\n", flag.Args())
-	if flag.NArg() == 0 {
+	Narg := flag.NArg()
+	if Narg == 0 {
 		fmt.Printf("need more arguments")
 		return
 	}
-	domain := flag.Arg(0)
-	ip, err := internal.GetAddress(domain)
-	if err == internal.OverMaxQueries {
-		fmt.Printf("Overstepped maxqueries, last ip %s\n", ip)
-		return
+	domains := flag.Args()
+	for i, domain := range domains {
+		ip, err := internal.GetAddress(domain)
+		if err == internal.OverMaxQueries {
+			fmt.Printf("%s Overstepped maxqueries, last ip %s\n", domain, ip)
+			continue
+		}
+		if err != nil {
+			fmt.Printf("Error quering %s: %s", domain, err)
+			continue
+		}
+		fmt.Printf("%s\n", ip)
+		if i+1 < Narg {
+			fmt.Print("\n")
+		}
 	}
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Printf("Ip address: %s\n", ip)
 }
