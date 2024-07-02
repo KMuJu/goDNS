325b6846016e503394815d6eb01a76215f89f688
Kasper <kasper.mustad.juell@linux.com>
2024-07-02 13:11:19 +0200

    Add possibility to compress multiple domains into one byte slice
    This is not correct however :(

--- internal/compression.go
+++ internal/compression.go
@@ -4,20 +4,32 @@ import (
 	"strings"
 )
 
-func CompressSingleDomain(domain string) []byte {
-	labels := strings.Split(domain, ".")
-	compressed := make([]byte, len(domain)+2)
-
-	index := 0
-	for _, s := range labels {
-		compressed[index] = byte(len(s))
-		index++
-		for _, c := range s {
-			compressed[index] = byte(c)
-			index++
-		}
+func getLength(domains []string) (length int) {
+	for _, d := range domains {
+		length += len(d) + 2
 	}
-	compressed[len(domain)+1] = 0
+	return length
+}
+
+// TODO: this is wrong, the compressed strings does not end in the same byte slice
+// It will be used for different questions and should maybe return a [][]byte???
+func CompressDomains(domains ...string) []byte {
+	compressed := make([]byte, getLength(domains))
+	index := 0
+	for _, domain := range domains {
+		labels := strings.Split(domain, ".")
+
+		for _, s := range labels {
+			compressed[index] = byte(len(s))
+			index++
+			for _, c := range s {
+				compressed[index] = byte(c)
+				index++
+			}
+		}
+		compressed[index] = 0
+		index++
+	}
 
 	return compressed
 }
